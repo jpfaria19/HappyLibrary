cb8de5f461fd59b30d018e739b26cac15074ce38
Jao <jpfaria19@hotmail.com>
2018-04-15 20:59:59 -0300

    Camada de dominio com Authors e Books

---
+++ BLL/Author.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BLL
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome."), Column(Order = 1)]
+        [MinLength(3, ErrorMessage = "O tamanho mínimo do nome são 3 caracteres.")]
+        [StringLength(200, ErrorMessage = "O tamanho máximo são 200 caracteres.")]
+        [DisplayName("Nome")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do autor."), Column(Order = 2)]
+        [MinLength(3, ErrorMessage = "O tamanho mínimo do sobrenome são 3 caracteres.")]
+        [StringLength(200, ErrorMessage = "O tamanho máximo são 200 caracteres.")]
+        [DisplayName("Sobrenome")]
+        public String Surname { get; set; }
+
+        [Required(ErrorMessage = "Digite seu e-mail"), Column(Order = 3)]
+        [DisplayName("E-mail")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail em formato inválido")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Digite a data de nascimento"), Column(Order = 4)]
+        [DisplayName("Data de nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Birthday { get; set; }
+        
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ BLL/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BLL
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do livro."), Column(Order = 1)]
+        [MinLength(3, ErrorMessage = "O tamanho mínimo do nome são 3 caracteres.")]
+        [StringLength(200, ErrorMessage = "O tamanho máximo são 200 caracteres.")]
+        [DisplayName("Título")]
+        public String Title { get; set; }
+
+        [Required(ErrorMessage = "Digite o número ISBN do livro."), Column(Order = 2)]
+        [MinLength(10, ErrorMessage = "O tamanho mínimo do nome são 10 caracteres.")]
+        [StringLength(17, ErrorMessage = "O tamanho máximo são 13 caracteres.")]
+        [DisplayName("ISBN")]
+        public String Isbn { get; set; }
+
+        [Required(ErrorMessage = "Digite a data de lançamento do livro"), Column(Order = 3)]
+        [DisplayName("Data de lançamento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Release { get; set; }
+
+        public virtual ICollection<Author> Authors { get; set; }
+    }
+}
