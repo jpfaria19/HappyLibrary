6ea200051e59ab1259d858cc66aaff5d59e49145
Jao <jpfaria19@hotmail.com>
2018-04-15 21:08:30 -0300

    Camada de servico ate o momento funcionando nas requisicoes da API;

---
+++ Service/AuthorService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using BLL;
+using Repositories.Repositories;
+
+namespace Service
+{
+    public class AuthorService
+    {
+        private readonly AuthorRepository _aR = new AuthorRepository();
+
+        public void AddBook(int authorId, Book book)
+        {
+            _aR.GetById(authorId).Books.Add(book);
+            _aR.Ct.SaveChanges();
+        }
+
+        public Author GetById(int id)
+        {
+            return _aR.GetById(id);
+        }
+
+        public IEnumerable<Author> GetAll()
+        {
+            return _aR.GetAll();
+        }
+
+        public void Add(Author obj)
+        {
+            _aR.Add(obj);
+        }
+
+        public void Update(Author obj)
+        {
+            _aR.Update(obj);
+        }
+
+        public void Remove(Author obj)
+        {
+            _aR.Remove(obj);
+        }
+
+        public void Dispose()
+        {
+            _aR.Dispose();
+        }
+    }
+}
---
+++ Service/BookService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using BLL;
+using Repositories.Repositories;
+
+namespace Service
+{
+    public class BookService
+    {
+        private readonly BookRepository _bR = new BookRepository();
+        private readonly AuthorRepository _aR = new AuthorRepository();
+
+        public void AddBook(int authorId, Book book)
+        {
+            //TODO: VERIFICAR SE ADD BOOKS ESTÁ CORRETO PASSANDO REPOSITÓRIO DE AUTHOR.
+
+            _aR.GetById(authorId).Books.Add(book);
+            _aR.Ct.SaveChanges();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bR.GetById(id);
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _bR.GetAll();
+        }
+
+        public void Add(Book obj)
+        {
+            _bR.Add(obj);
+        }
+
+        public void Update(Book obj)
+        {
+            _bR.Update(obj);
+        }
+
+        public void Remove(Book obj)
+        {
+            _bR.Remove(obj);
+        }
+
+        public void Dispose()
+        {
+            _bR.Dispose();
+        }
+    }
+}
